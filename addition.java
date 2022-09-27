25acee960219bfcee1fe9350ca1b30fd86963523
Ramlakhan <ramlakhanlodhi229@gmail.com>
2022-09-27 23:28:40 +0530

    Additon Commit

---
+++ addition.java
@@ -0,0 +1,12 @@
+class Addition{
+        int a=10;
+        int b=25;
+        int c;
+    public static void main(String a[])
+    {
+        Addition obj=new Addition();        
+         obj.c=obj.a+obj.b;       
+        System.out.println("Addition is "+(obj.c));
+        
+    }
+}
