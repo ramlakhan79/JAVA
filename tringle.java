004949367cad681f6be58adeaa7e4c0195227887
Ramlakhan <ramlakhanlodhi229@gmail.com>
2022-09-28 23:39:06 +0530

    Tringle Commit

---
+++ tringle.java
@@ -0,0 +1,9 @@
+class Tringle {
+    public static void main(String[] args) {
+         int b=10;
+         int a=15;
+         int A;
+         A=(a*b)/2;
+        System.out.println("Area is "+A);
+    }
+}
