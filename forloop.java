67a3e90ec069c948c2a17b869ae64f736042908a
Ramlakhan <ramlakhanlodhi229@gmail.com>
2022-10-05 20:44:59 +0530

    Forloop Commit

---
+++ forloop.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;  
+public class forloop
+{  
+        public static void main(String a[])   
+          {  
+               Scanner sc = new Scanner(System.in);  
+               System.out.print("Enter number: ");      
+           int num=sc.nextInt();
+            for(int i=0; i<=num; i++)
+             {
+                        System.out.println(i);  
+             }                 
+    }  
+}  
+
