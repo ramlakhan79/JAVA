382d77a0678059b6348beb5416430fdb80de1782
Ramlakhan <ramlakhanlodhi229@gmail.com>
2022-10-06 22:43:29 +0530

    Subtraction Commit

---
+++ subtraction.java
@@ -0,0 +1,14 @@
+public class subtraction {
+    
+    int a=50;
+    int b=35;
+    int c;
+public static void main(String a[])
+{
+    subtraction obj=new subtraction();        
+     obj.c=obj.a-obj.b;       
+    System.out.println("subtraction is "+(obj.c));
+    
+}
+}
+
