1c07146d44536adade03b665f4b519893a0bb861
Ramlakhan <ramlakhanlodhi229@gmail.com>
2022-11-27 20:02:42 +0530

    Table

---
+++ Table.java
@@ -0,0 +1,9 @@
+public class Table {
+    public static void main(String arg[]){
+        int t=3;
+        for(int i=1;i<=10;i++){
+            System.out.print((t*i)+"\n");
+        }
+    }
+    
+}
