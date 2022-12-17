b2b7f777147a71c6c03409ac45e89135e7512117
Ramlakhan Lodhi <112104877+ramlakhan79@users.noreply.github.com>
2022-12-17 21:11:42 +0530

    Scanner

---
+++ ScannerExample.java
@@ -0,0 +1,20 @@
+/*  Aim: Write a program in JAVA to take 3 numbers as Input from User, compute Average and 
+display the result. */
+
+import java.util.*;
+class ScannerExample
+{
+public static void main(String[] args)
+{
+int a = 0, b = 0, c = 0, s = 0;
+double avg = 0.0;
+Scanner scan = new Scanner(System.in);
+System.out.println("Enter Three Integers : ");
+a = scan.nextInt();
+b = scan.nextInt();
+c = scan.nextInt();
+s = a + b + c;
+avg = s / 3.0;
+System.out.println("Average of Three Numbers is : " + avg);
+}
+}
